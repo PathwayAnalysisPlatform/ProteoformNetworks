f45aa8bfc1849df416fe2b8855f1d5e10c8c7231
Luis Francisco Hernández Sánchez <franciscohsmex@gmail.com>
2022-04-20 00:22:24 +0200

    Fix calculations of size and degree

--- src/Python/config.py
+++ src/Python/config.py
@@ -46,6 +46,7 @@ URL_PATHWAYMATCHER = "https://github.com/PathwayAnalysisPlatform/PathwayMatcher/
 DATA_REACTOME_PATH = "data/Reactome/79/"
 NETWORKS_PATH = "networks/"
 MAPPING_FILE = "mapping_proteins_to_level.tsv"
+PATHWAY_GRAPHS_PATH = "networks/pathways/"
 
 
 def set_root_wd():
